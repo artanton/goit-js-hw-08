e44d5dd48fd08e29ba65477bca77507e4335813f
artanton <kabletrade@gmail.com>
2023-08-12 23:59:31 +0300

    hw8-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on ('timeupdate', throttle(timeChecker,1000));
+
+function timeChecker (data){
+    localStorage.setItem("KURRENT TIME", data.seconds);};
+
+    const videoCurrentTime = localStorage.getItem("KURRENT TIME")||0;
+
+    player.setCurrentTime(videoCurrentTime);
