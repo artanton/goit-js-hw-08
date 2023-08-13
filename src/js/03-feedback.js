11f4728e6308fe3c43a242f8704587c9a422400d
artanton <kabletrade@gmail.com>
2023-08-13 17:12:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,7 +11,7 @@ const data = {
   message: "",
 };
 
-
+// Load saved data from local storage on page load
 const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
 if (savedData) {
   refs.inputEmail.value = savedData.email;
@@ -31,7 +31,7 @@ function dataHandler() {
 refs.form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
-  event.preventDefault(); 
+  event.preventDefault();
   localStorage.removeItem('feedback-form-state');
   resetForm(refs.form); 
   console.log(data);
